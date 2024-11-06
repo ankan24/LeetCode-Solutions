7f98029be23850063e30acb04c59683216c2fb47
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-06 23:42:21 +0530

    Time: 0 ms (100%), Space: 9.3 MB (77.18%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
-        for(int i=0;i<n+1;i++){
-            ans[i] = __builtin_popcount(i);
+        for(int i=1;i<=n;i++){
+            if(i%2 != 0){
+                ans[i] = ans[i/2]+1;
+            }else{
+                ans[i] = ans[i/2];
+            }
         }
         return ans;
     }
