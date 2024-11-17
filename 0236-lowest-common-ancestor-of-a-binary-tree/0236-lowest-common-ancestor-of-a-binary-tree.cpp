ced52b1f55198db5d71ec47372a5a67715bfa52f
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-17 15:05:34 +0530

    Time: 14 ms (41.25%), Space: 16.1 MB (91.91%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -16,10 +16,8 @@ public:
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
  
-        if (left != NULL && right != NULL) {
-            return root;
-        }
-
-        return left != NULL ? left : right;
+         if(left==NULL) return right;
+         if(right==NULL) return left;
+         return root;
     }
 };
