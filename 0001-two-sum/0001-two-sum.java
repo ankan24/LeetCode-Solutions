e900c9ffa30d44ba497156ebd87baa1eb9d385f3
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2024-11-18 20:00:07 +0530

    Time: 50 ms (10.14%), Space: 44.6 MB (78.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
+         int[] ans = new int[2];
         int len = nums.length;
         for(int i=0;i<len;i++){
             for(int j=i+1;j<len;j++){
@@ -11,5 +11,6 @@ class Solution {
             }
         }
         return ans;
+        
     }
 }
