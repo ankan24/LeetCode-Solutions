7093900e4a4377619c9f45d11b75c254fcc3e261
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-20 15:44:32 +0530

    Time: 6 ms (86.66%), Space: 51.5 MB (45.26%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void backtrack(int[] nums, int index, List<List<Integer>>finalAnswer,List<Integer>ans){
+        if(ans.size() >= 2) finalAnswer.add(new ArrayList<>(ans));
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=index;i<nums.length;i++){
+            if((ans.size() == 0 || ans.get(ans.size()-1)<= nums[i]) && !set.contains(nums[i])){
+                ans.add(nums[i]);
+                backtrack(nums,i+1,finalAnswer,ans);
+                ans.remove(ans.size()-1);
+                set.add(nums[i]);
+            }
+        }
+    }
+    public List<List<Integer>> findSubsequences(int[] nums) {
+            List<List<Integer>>finalAnswer = new ArrayList<>();
+            List<Integer>ans = new ArrayList<>();
+            backtrack(nums,0,finalAnswer,ans);
+            return finalAnswer;    
+    }
+}
