14d531fd5900d5d5fc62bff72134c2521dfc4fad
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-24 00:54:59 +0530

    Time: 1 ms (42.6%), Space: 40.9 MB (67.84%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,43 +1,32 @@
 class Solution {
-public int findPath(int i,int j, int[][] g , int zc){
-    // Invalid Conditions - 
-    if(i<0 || j<0 || i >= g.length || j >= g[0].length || g[i][j] == -1){
-        return 0;
-    }
+    public int path(int i,int j, int[][] g,int zc){
+    if(i<0 || j<0 || i>=g.length || j>=g[0].length || g[i][j]==-1) return 0;
 
-    // Goal -
-    if(g[i][j]==2){
-       return (zc==0) ? 1 : 0;
-    }
-   
+    if(g[i][j] == 2 && zc==0) return 1;
+
     int temp = g[i][j];
     g[i][j] = -1;
-    int nzc = (temp==0) ? zc-1 : zc;
-
-    int ans=findPath(i+1,j,g,nzc)+findPath(i-1,j,g,nzc)+findPath(i,j+1,g,nzc)+findPath(i,j-1,g,nzc);
-
+    int nzc = (temp==0) ? zc-1 : zc;  
+    int ans = path(i+1,j,g,nzc)+ path(i-1,j,g,nzc)+ path(i,j+1,g,nzc)+ path(i,j-1,g,nzc);
     g[i][j] = temp;
     return ans;
-}
-  
-public int uniquePathsIII(int[][] grid) {
-          int zc = 0;
-          for(int i=0;i<grid.length;i++){
+    }
+
+    public int uniquePathsIII(int[][] grid) {
+        int zc = 0;
+        for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==0){  
-                    zc++;
-                }
+                if(grid[i][j]==0) zc++;
             }
-          }
-         int  ans = 0;
+        }
+         int ans = 0;
          for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1){
-                    ans += findPath(i,j,grid,zc);
+                    ans += path(i,j,grid,zc);
                 }
             }
-          }
-
-       return ans;
+        }
+        return ans;
     }
 }
