8d28ba7a214799311ad48f020297d48419648701
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-24 01:17:32 +0530

    Time: 6 ms (86.93%), Space: 51.1 MB (75.42%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,12 +1,12 @@
 class Solution {
     public void backtrack(int[] nums, int index, List<List<Integer>>finalAnswer,List<Integer>ans){
-        if(ans.size() >= 2) finalAnswer.add(new ArrayList<>(ans));
+    if(ans.size() >= 2) finalAnswer.add(new ArrayList<>(ans));
         HashSet<Integer> set = new HashSet<>();
         for(int i=index;i<nums.length;i++){
-            if((ans.size() == 0 || ans.get(ans.size()-1)<= nums[i]) && !set.contains(nums[i])){
+    if((ans.size() == 0 || ans.get(ans.size()-1)<= nums[i]) && !set.contains(nums[i])){
                 ans.add(nums[i]);
                 backtrack(nums,i+1,finalAnswer,ans);
-                ans.remove(ans.size()-1);
+                ans.remove(ans.size()-1); 
                 set.add(nums[i]);
             }
         }
