1d24a81efc1357b86b39daa38985235af6683950
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-23 20:01:49 +0530

    Time: 127 ms (90.51%), Space: 41.7 MB (47.79%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,29 @@
 class Solution {
-    public boolean findWord(int i, int j, int index , char[][] b , String w){
-         // Goal -
-         if(index == w.length()){
+    public boolean find(int i,int j,int index,char[][] b,String w){
+        if(index == w.length()){
             return true;
-         }
-         // Invalid Conditions - 
-         if(i<0 || j<0 || i>= b.length || j>=b[0].length || b[i][j]!= w.charAt(index) || b[i][j] =='$'){
+        }
+        if(i<0 || j<0 || i>=b.length || j>=b[0].length || b[i][j]!=w.charAt(index)){
             return false;
-         }
-
+        }
         char temp = b[i][j];
         b[i][j] = '$';
-         // Explore all paths -
-        boolean ans = findWord(i+1,j,index+1,b,w) ||
-         findWord(i-1,j,index+1,b,w) ||
-         findWord(i,j+1,index+1,b,w) ||
-         findWord(i,j-1,index+1,b,w);
-         b[i][j] = temp;
-        return ans;
 
+        boolean ans = find(i+1,j,index+1,b,w) || find(i,j+1,index+1,b,w) ||
+                      find(i-1,j,index+1,b,w) || find(i,j-1,index+1,b,w);
+
+        b[i][j] = temp;
+       return ans;
     }
 
     public boolean exist(char[][] board, String word) {
-        for(int i=0 ; i< board.length ; i++){
-            for(int j=0; j<board[0].length;j++){
-             if(board[i][j] == word.charAt(0) &&  findWord(i,j,0,board,word)){
-               return true;
-              }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(find(i,j,0,board,word)){
+                        return true;
+                    }
+                }
             }
         }
         return false;
