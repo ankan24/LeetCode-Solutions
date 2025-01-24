5a2ebeed37ebf495f1a62e5dce3040fafbd845bd
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-24 19:56:14 +0530

    Time: 0 ms (100%), Space: 22 MB (13.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-
-        int num1 = 0 , num2 = 0;
-        for(int i=0;i<n;i++){
-            num1 += nums[i];
+        sort(nums.begin(),nums.end());
+        int mx = nums[n-1];
+        int i;
+        for(i=0;i<mx;i++){
+            if(nums[i]!=i) return i;
         }
-        for(int j=0;j<n+1;j++){
-            num2+=j;
-        }
-        return num2-num1;
-        
+        return i+1;
     }
 };
