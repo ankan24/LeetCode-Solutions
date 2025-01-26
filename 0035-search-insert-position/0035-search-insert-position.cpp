8069be61ee9798652dced4e7f6c9344cf4131df6
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-01-26 16:29:33 +0530

    Time: 0 ms (100%), Space: 13.6 MB (74.91%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,7 +3,6 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int l = 0;
         int r = nums.size()-1;
-        int ans = -1;
         if(nums[r] < target) return r+1;
         while(l <= r){
             int m = l+(r-l)/2;
@@ -11,10 +10,9 @@ public:
             else if(nums[m] < target){ 
                l = m+1;
             }else{
-                ans = m;
                 r = m-1;
             }
         }
-        return ans;
+        return l;
     }
 };
