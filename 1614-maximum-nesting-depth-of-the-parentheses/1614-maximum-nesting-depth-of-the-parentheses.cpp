6550fd8fd3f791d8089bb646e916391ae2372902
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-02-01 22:16:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.46%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     int maxDepth(string s) {
         int c = 0;
         int ans = 0;
-        for(int i=s.length();i>=0;i--){
-            if(s[i]==')'){
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
                 c++;
             }
             if(ans < c){
                 ans = c;
             }
-            if(s[i]=='('){
+            if(s[i]==')'){
                 c--;
             }
         }
