c18940fffcf5b244c8522ba62ac5e0b3612de47b
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-02-16 20:05:54 +0530

    Time: 44 ms (8.99%), Space: 49.7 MB (8.14%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -5,7 +5,7 @@ public:
          // we dont need to check right , right-up , right-down . cause we dont filled it yet.
          // check left-up side
          while(row >= 0 && col >= 0){
-            if(board[row][col]=='Q') return false;
+            if(board[row][col]=='Q') return false; //if Queen already exist on this side then this place is incorrect
             row--;
             col--;
          }
@@ -13,7 +13,7 @@ public:
          row = r1;
          col = c1;
          while(col >= 0){
-            if(board[row][col]=='Q') return false;
+            if(board[row][col]=='Q') return false; //if Queen already exist on this side then this place is incorrect
             col--;
          }
 
@@ -21,7 +21,7 @@ public:
          row = r1;
          col = c1;
          while(col >= 0 && row < n){
-            if(board[row][col]=='Q') return false;
+            if(board[row][col]=='Q') return false; //if Queen already exist on this side then this place is incorrect
             col--;
             row++;
          }
@@ -29,28 +29,28 @@ public:
     }
 
     void solve(int col ,vector<string>& board, vector<vector<string>>& ans, int n){
-        if(col == n){
-            ans.push_back(board);
-            return;
+        if(col == n){ // if column size == the bord size then a correct posible board is created
+            ans.push_back(board);  // add the board on answer
+            return; 
         }    
 
-        for(int row = 0 ; row < n ;row++){
-            if(isSafe(row,col,board,n)){
-                board[row][col] = 'Q';
-                solve(col+1 , board, ans, n);
-                board[row][col] = '.';
+        for(int row = 0 ; row < n ;row++){    // start from colum-0 and row-0
+            if(isSafe(row,col,board,n)){   // check is the position is safe
+                board[row][col] = 'Q'; // if safe fill the Queen
+                solve(col+1 , board, ans, n);  // then go to the next column
+                board[row][col] = '.'; // backtrack and fill it previous value
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n , '.');
+        vector<vector<string>> ans; // store all posible positions
+        vector<string> board(n); 
+        string s(n , '.'); // inisilize a row of .
         for(int i=0;i<n;i++){
-            board[i] = s;
+            board[i] = s; // inisilize full bord N*N with .
         }
-        solve(0,board,ans,n);
+        solve(0,board,ans,n); //start from colum 0. the function will find which row is correct position
         return ans;
     }
 };
