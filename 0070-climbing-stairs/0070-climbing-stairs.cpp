f647617122e6c1f1c343b18da181a933a2cbf3b4
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-02-25 15:06:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,7 +10,7 @@ public:
 
        for(int i=3;i<=n;i++){
         arr[i] = arr[i-1]+arr[i-2];
-       }
+       }                                                                   
         return arr[n];
-    }
+    }                  
 };
