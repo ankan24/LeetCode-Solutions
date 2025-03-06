d265f8b434f39b4ca4fc6743898895b474bbcc8a
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-03-06 23:19:38 +0530

    Time: 0 ms (100%), Space: 11.6 MB (69.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] > nums[j]){
-                    swap(nums[i],nums[j]);
-                }
+       // Dutch National Flag Algo
+
+        int low = 0 , mid = 0 , high = nums.size()-1;
+
+        while(mid <= high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
     }
