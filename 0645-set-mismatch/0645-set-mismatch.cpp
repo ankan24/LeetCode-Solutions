69d7a1599c608703ca47cc8dfc1951701ba9540a
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-03-09 15:08:00 +0530

    Time: 69 ms (9.35%), Space: 36.8 MB (7.27%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(2,0);
+            vector<int> ans(2,0);
         map<int,int> mp;
         for(int ele : nums){
             mp[ele]++;
         }
 
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=nums.size();i++){
             if(mp[i]==2) ans[0] = i;
             if(mp[i]==0) ans[1] = i;
         }
-        if(ans[0]==0) ans[0] = nums[n-2];
-        if(ans[1]==0) ans[1] = n;
         return ans;
     }
 };
