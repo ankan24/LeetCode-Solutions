0c9a77f6104e32877861653aeabc31b14ff9a342
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-03-13 11:29:08 +0530

    Time: 0 ms (100%), Space: 20 MB (8.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         map<int,int> mp;
-
         for(int i=0;i<n;i++){
-         mp[nums[i]]++;
+            mp[nums[i]]++;
         }
-         vector<int> ans;
-        for(auto it : mp){
-            if(it.second > n/3){
-               ans.push_back(it.first);
-            } 
+
+        vector<int> ans;
+        for(auto ele : mp){
+            if(ele.second > n/3) ans.push_back(ele.first);
         }
         return ans;
     }
