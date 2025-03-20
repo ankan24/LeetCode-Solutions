aadac9abebd35f3ccf4b82225ec313bb50e9c1f4
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-03-20 19:27:49 +0530

    Time: 0 ms (100%), Space: 16.7 MB (31.86%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,27 +11,23 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==0 ) return head;
-        ListNode* temp = head;
+        if(head==NULL || head->next==NULL || k==0) return head;
+        ListNode* t = head;
         vector<int> arr;
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        while(t!=NULL){
+            arr.push_back(t->val);
+            t = t->next;
         }
-        k = k%arr.size();
-        if(k==0) return head;
+         k = k%arr.size();
         vector<int> ans;
-        for(int i=arr.size()-k;i<arr.size();i++){
-            ans.push_back(arr[i]);
-        }
-        for(int i=0;i<arr.size()-k;i++){
-             ans.push_back(arr[i]);
-        }
-        temp = head;
-        for(int i=0;i<ans.size();i++){
-            temp->val = ans[i];
-            temp = temp->next;
-        }
-        return head;
+        for(int i = arr.size()-k;i<arr.size();i++) ans.push_back(arr[i]);
+        for(int i=0;i<arr.size()-k;i++) ans.push_back(arr[i]);
+
+        t = head;
+       for(int i=0;i<ans.size();i++){
+          t->val = ans[i];
+          t = t->next;
+       }
+       return head;
     }
 };
