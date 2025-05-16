fddae2f5f9e2f4e83df758e524f9db319d201dbe
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-05-16 20:55:49 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void findCombinations(string& digits,string s,int idx, vector<string> arr , vector<string>& ans){
-           if(digits.length() == s.length()){
-              ans.push_back(s);
-              return;
-           }
-
-       int num = digits[idx]-'0';
-       string letter = arr[num];
-        for(int i=0;i<letter.length();i++){
-            findCombinations(digits,s+letter[i],idx+1,arr,ans);
+    void solve(vector<string>& ans, string op, int i, string mp[], string d){
+           // base case 
+        if(i >= d.length()){
+            ans.push_back(op);
+            return;
         }
+        
+         int num = d[i]-'0';
+         string val = mp[num];
+
+         for(int j=0;j<val.length();j++){
+            op.push_back(val[j]);
+            solve(ans,op,i+1,mp,d);
+            op.pop_back();
+         }
     }
 
     vector<string> letterCombinations(string digits) {
         if(digits.length()==0) return {};
-        vector<string> arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string> ans;
-        string s;
-        findCombinations(digits,s,0,arr,ans);
+        string op;
+        int i = 0;
+        string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(ans,op,i,mapping,digits);
         return ans;
     }
 };
