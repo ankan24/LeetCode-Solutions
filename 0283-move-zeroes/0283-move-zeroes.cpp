580a1b35db5926e82214eff40aac79c028a33a23
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-06 01:28:09 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int idx = 0;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0) nums[idx++] = nums[i];
+        int zc = 0;
+        for(int i=0;i<nums.size();){
+            if(nums[i]==0){
+                nums.erase(nums.begin()+i);
+                zc++;
+            }else{
+                i++;
+            }
         }
-        while(n > idx){
-            nums[idx++] = 0;
+        for(int i=0;i<zc;i++){
+            nums.push_back(0);
         }
     }
 };
