2697d6fcfb31c30977579ac3dcc93359e8f10897
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-06 12:34:26 +0530

    Time: 2 ms (44.35%), Space: 42.2 MB (25.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
-        int n = nums.size();
-        vector<int> ans2(n,0);
-        int prod = 1; 
-        int zc = 0;
-        for(int i=0;i<n;i++){
-           prod *= nums[i];
-           if(nums[i]==0)
-           zc++;
-        }
-        if(zc == n) return nums;
-        if(zc > 1) return ans2;
-        int zprod = 1;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0)
-               zprod *= nums[i];
-        }
+      int n = nums.size();
+       vector<int> l(n);
+       vector<int> r(n);
+       vector<int> ans(n);
 
-          for(int i=0;i<n;i++){
-           if(nums[i]!=0)
-           ans.push_back(prod/nums[i]);
-           else {
-            if(nums[i]==0) ans.push_back(zprod);
-            else ans.push_back(0);
-           }
-          }
+       l[0] = 1;
+       for(int i=1;i<n;i++){
+        l[i] = l[i-1]*nums[i-1];
+       } 
 
-        return ans;
+       r[n-1] = 1;
+       for(int i=n-2;i>=0;i--){
+        r[i] = r[i+1]*nums[i+1];
+       }
+
+       for(int i=0;i<n;i++){
+        ans[i] = l[i]*r[i];
+       }
+
+       return ans;
     }
 };
