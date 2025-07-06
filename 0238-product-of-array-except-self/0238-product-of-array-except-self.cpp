b7e97af829314ad5d04ff45106c9e603982f4422
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-06 11:42:12 +0530

    Time: 4 ms (21.81%), Space: 42.8 MB (7.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int mul = 1 , zc=0;;
+        vector<int> ans;
         int n = nums.size();
+        vector<int> ans2(n,0);
+        int prod = 1; 
+        int zc = 0;
         for(int i=0;i<n;i++){
+           prod *= nums[i];
+           if(nums[i]==0)
+           zc++;
+        }
+        if(zc == n) return nums;
+        if(zc > 1) return ans2;
+        int zprod = 1;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+               zprod *= nums[i];
+        }
+
+          for(int i=0;i<n;i++){
            if(nums[i]!=0)
-            mul *= nums[i];
-            else
-            zc++;
-        }
-        for(int i=0;i<n;i++){
-            if(zc>1)
-             nums[i] = 0; 
-           else if(zc==1)
-            nums[i]=(nums[i] == 0)?mul:0;
-           else
-              nums[i] = mul/nums[i];
-        }
-        return nums;
+           ans.push_back(prod/nums[i]);
+           else {
+            if(nums[i]==0) ans.push_back(zprod);
+            else ans.push_back(0);
+           }
+          }
+
+        return ans;
     }
 };
