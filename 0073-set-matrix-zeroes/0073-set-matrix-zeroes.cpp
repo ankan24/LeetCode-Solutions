f784ea86e0a0411e207fc7f4393bba6436b81578
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-07 21:23:31 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> row(m,0);   // record all rows , where 0 found make the index 1
-        vector<int> col(n,0);   // record all colums , where 0 found make the index 1
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    row[i] = 1;   // make 1 where 0 found
-                    col[j] = 1;   // make 1 whare 0 found
+    void setZeroes(vector<vector<int>>& mtx) {
+        int n = mtx.size();
+        int m = mtx[0].size();
+
+        vector<int> r(n,0);
+        vector<int> c(m,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mtx[i][j]==0){ 
+                    r[i] = 1;
+                    c[j] = 1;
                 }
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i]==1 || col[j]==1){ // where 1 found make them 0
-                    matrix[i][j] = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(r[i]==1 || c[j]==1){
+                    mtx[i][j]=0;
                 }
             }
         }
