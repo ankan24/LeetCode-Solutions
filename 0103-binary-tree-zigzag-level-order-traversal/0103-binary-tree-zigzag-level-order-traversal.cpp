2855e52829f4782f1ae84fe81c0860d4e2ab9b1d
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-11 02:19:56 +0530

    Time: 0 ms (100%), Space: 15.2 MB (18.21%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,40 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if(root==NULL) return ans;
-
+        if (!root)
+            return ans;
         queue<TreeNode*> q;
         q.push(root);
-         int level = 0;
+        int lev = 0;
+        while (!q.empty()) {
+            vector<int> st;
+            int n = q.size();
 
-        while(!q.empty()){
-            level++;
-            int size = q.size();
-            vector<int> store;
-          for(int i=0;i<size;i++){
-            TreeNode* node = q.front();
-            q.pop();
-            store.push_back(node->val);
+            for (int i = 0; i < n; i++) {
+                TreeNode* t = q.front();
+                q.pop();
+                st.push_back(t->val);
+                if (t->left)
+                    q.push(t->left);
+                if (t->right)
+                    q.push(t->right);
+            }
 
-            if(node->left){
-                q.push(node->left);
+            if (lev % 2 != 0) {
+                reverse(st.begin(), st.end());
             }
-            if(node->right){
-                q.push(node->right);
-            }
-          }
-          if(level % 2 == 0){
-            reverse(store.begin(),store.end());
-          }
-          ans.push_back(store);
+            lev++;
+            ans.push_back(st);
         }
-       return ans;   
+        return ans;
     }
 };
