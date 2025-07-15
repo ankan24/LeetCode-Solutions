0f36dfbb47f7c711e69bd8ac1bbd1ce890cdea34
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-15 15:39:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.63%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     bool isHappy(int n) {
-       int sum =0;
-      while(1){
-       while(n > 0){
-        int rem = n%10;
-        sum += pow(rem,2);
-        n /= 10;
-       }
-      if(sum==1 || sum/10==0 && sum!=7){
-        break;
-      }
-      n = sum;
-      sum = 0;
-      }
-      if(sum == 1){
-        return true;
-      }else{
-        return false;
-      }
+         if(n==1 || n==7) return true;
+        else if(n<10) return false;
+        else{
+            int sum=0;
+            while(n>0){
+                int temp=n%10;
+                sum+= temp*temp;
+                n=n/10;
+            }
+            return isHappy(sum);
+        }
     }
 };
