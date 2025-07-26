84a3f7496e16e701811e9ab09a653aa4b69f8f60
Ankan Ghorai <138057985+ankan24@users.noreply.github.com>
2025-07-26 18:58:25 +0530

    Time: 3 ms (66.55%), Space: 8.1 MB (20.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int fibo(vector<int>& dp , int n){
-        if(n==0 || n==1) return n;
-        if(dp[n]!=-1) return dp[n];
-        dp[n] = fibo(dp,n-1)+fibo(dp,n-2);
+    int solve(int n, vector<int>& dp){
+         if(n==0 || n==1) return n;
+         if(dp[n]!=-1) return dp[n];
+         dp[n] = solve(n-1,dp)+solve(n-2,dp);
          return dp[n];
     }
     int fib(int n) {
-        vector<int> dp(n+1 , -1);
-        int ans = fibo(dp , n);
-        return ans;
+        if(n==0 || n==1) return n;
+        vector<int> dp(n+1,-1);
+        solve(n,dp);
+        return dp[n];
     }
 };
